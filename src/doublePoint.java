493d5e1ecf5028e8ba8533cb1ebdc1cae47dd16b
Matthew Pearce <pearcematthew25@gmail.com>
2015-06-02 11:15:44 -0400

    adding util classes

---
+++ src/doublePoint.java
@@ -0,0 +1,32 @@
+public class doublePoint {
+
+	private Double x, y;
+
+	public doublePoint(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public int getIntX() {
+		return x.intValue();
+	}
+
+	public int getIntY() {
+		return y.intValue();
+	}
+
+	////set methods
+	public void setPoints(double x, double y){
+		this.x = x;
+		this.y = y;
+	}
+}
